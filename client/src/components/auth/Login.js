f06514ceb46762d87882510d986d86c7bb018b3a
Adrian <adrberia@gmail.com>
2020-05-13 01:13:31 -0400

    Added bootstrap, auth and layout components

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (setter) => (e) => setter(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const newUser = { email, password };
+    console.log(newUser);
+  };
+
+  return (
+    <div className="login">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h1 className="display-4 text-center">Log In</h1>
+            <p className="lead text-center">
+              Sign in to your DevUnited account
+            </p>
+            <form onSubmit={onSubmit}>
+              <div className="form-group">
+                <input
+                  type="email"
+                  className="form-control form-control-lg"
+                  placeholder="Email Address"
+                  name="email"
+                  value={email}
+                  onChange={onChange(setEmail)}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control form-control-lg"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={onChange(setPassword)}
+                />
+              </div>
+              <input type="submit" className="btn btn-info btn-block mt-4" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+
+  const onChange = (setter) => (e) => setter(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const newUser = { name, email, password, password2 };
+    console.log(newUser);
+  };
+
+  return (
+    <div className="register">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <h1 className="display-4 text-center">Sign Up</h1>
+            <p className="lead text-center">Create your DevConnector account</p>
+            <form onSubmit={onSubmit}>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  placeholder="Name"
+                  name="name"
+                  value={name}
+                  onChange={onChange(setName)}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="email"
+                  className="form-control form-control-lg"
+                  placeholder="Email Address"
+                  name="email"
+                  value={email}
+                  onChange={onChange(setEmail)}
+                />
+                <small className="form-text text-muted">
+                  This site uses Gravatar so if you want a profile image, use a
+                  Gravatar email
+                </small>
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control form-control-lg"
+                  placeholder="Password"
+                  name="password"
+                  value={password}
+                  onChange={onChange(setPassword)}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control form-control-lg"
+                  placeholder="Confirm Password"
+                  name="password2"
+                  value={password2}
+                  onChange={onChange(setPassword2)}
+                />
+              </div>
+              <input type="submit" className="btn btn-info btn-block mt-4" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
